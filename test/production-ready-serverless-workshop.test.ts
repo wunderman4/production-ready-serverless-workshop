08ded124de1db758a4733375023299444d321493
Christian Wunder <christianwunder@Christians-MacBook-Pro.local>
2024-03-22 14:35:59 -0700

    Initial commit

---
+++ test/production-ready-serverless-workshop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ProductionReadyServerlessWorkshop from '../lib/production-ready-serverless-workshop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/production-ready-serverless-workshop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ProductionReadyServerlessWorkshop.ProductionReadyServerlessWorkshopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
